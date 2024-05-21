9767112e2f09967bb9111a1ceaf51ff490359bc3
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2024-05-21 15:17:44 +0000

    Datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //using NodeJS, not browser
+
+console.log(3+3); //code readability should be high
+console.log("SaumyaShah");
+
+let name = "saumya";
+let age = 20;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof undefined); //undefined's type is undefined
+console.log(typeof null); //null is an object
