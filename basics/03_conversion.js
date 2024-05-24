ef9e4bcd5f74a9e6b5da95e70bb6385c8dfd8759
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2024-05-24 13:48:09 +0000

    Conversion of data typr

---
+++ basics/03_conversion.js
@@ -0,0 +1,30 @@
+let score = "dtfhvub";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefined => NaN
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "saumya" => true
+
+let someNumber = 33
+
+let stringNum = String(someNumber)
+
+console.log(stringNum);
+console.log(typeof stringNum);
